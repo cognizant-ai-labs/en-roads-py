7598f510fe13939ffa87f4121d33f0e0b16a3459
Daniel Young <danyoung@utexas.edu>
2024-10-28 11:51:20 -0700

    Added non-timeline actions to actions view

--- app/components/link.py
+++ app/components/link.py
@@ -21,8 +21,9 @@ class LinkComponent():
     Component in charge of displaying the links to En-ROADS.
     """
 
-    def __init__(self, cand_idxs: list[int]):
-        self.cand_idxs = cand_idxs
+    def __init__(self, cand_idxs: list[int], actions: list[str]):
+        self.cand_idxs = [i for i in cand_idxs]
+        self.actions = [a for a in actions]
 
         self.colors = ["brown", "red", "blue", "green", "pink", "lightblue", "orange"]
         self.energies = ["coal", "oil", "gas", "renew and hydro", "bio", "nuclear", "new tech"]
@@ -30,8 +31,16 @@ class LinkComponent():
 
         with open("app/categories.json", "r", encoding="utf-8") as f:
             self.categories = json.load(f)
+
+        # Pre-compute timeline actions for later when we show actions
         with open("app/timeline.yaml", "r", encoding="utf-8") as f:
             self.timeline = yaml.load(f, Loader=yaml.FullLoader)
+        self.timeline_actions = []
+        for action, details in self.timeline.items():
+            self.timeline_actions.append(action)
+            for d in details.values():
+                self.timeline_actions.append(d)
+
         self.input_specs = load_input_specs()
 
     def plot_energy_policy(self, energy_policy_jsonl: list[dict[str, list]], cand_idx: int) -> go.Figure:
@@ -84,31 +93,10 @@ class LinkComponent():
         )
         return fig
 
-    def translate_context_actions_dict(self, context_actions_dict: dict[str, float]) -> html.Div:
-        """
-        Translates a context actions dict into a nice div to display
-        """
-        children = []
-        for category in self.categories:
-            children.append(html.H4(category))
-            remove = True  # If we don't have any actions in this category, remove it
-            for action in self.categories[category]:
-                if action in context_actions_dict:
-                    remove = False
-                    input_spec = self.input_specs[self.input_specs["varId"] == action].iloc[0]
-                    val = context_actions_dict[action]
-                    if input_spec["kind"] == "slider":
-                        formatting = input_spec["format"]
-                        val_formatted = f"{val:{formatting}}"
-                    else:
-                        val_formatted = "on" if val else "off"
-                    children.append(html.P(f"{input_spec['varName']}: {val_formatted}"))
-            if remove:
-                children.pop()
-
-        return html.Div(children)
-
-    def _create_timeline_events(self, action, details, context_actions_dict):
+    def _create_timeline_events(self,
+                                action: str,
+                                details: dict,
+                                context_actions_dict: dict[str, float]) -> dict[int, str]:
         """
         Creates 0 or more timeline events for a given action.
         We have to manually handle electric standard being active and if this is the final carbon tax as they rely on
@@ -152,15 +140,30 @@ class LinkComponent():
         """
         timeline = defaultdict(list)
         for action, details in self.timeline.items():
-            events = self._create_timeline_events(action, details, context_actions_dict)
-            for year, event in events.items():
-                timeline[year].append(event)
+            if action in context_actions_dict:
+                events = self._create_timeline_events(action, details, context_actions_dict)
+                for year, event in events.items():
+                    timeline[year].append(event)
 
-        children = []
+        children = [html.H3("Timeline")]
         for year in sorted(timeline.keys()):
             children.append(html.H4(str(year)))
             for event in timeline[year]:
                 children.append(html.P(event))
+
+        non_timeline = [html.H3("Initial Actions")]
+        for action in context_actions_dict:
+            if action not in self.timeline_actions and action in self.actions:
+                input_spec = self.input_specs[self.input_specs["varId"] == action].iloc[0]
+                val = context_actions_dict[action]
+                if input_spec["kind"] == "slider":
+                    formatting = input_spec["format"]
+                    val_formatted = f"{val:{formatting}}"
+                else:
+                    val_formatted = "on" if val else "off"
+                non_timeline.append(html.P(f"{input_spec['varName']}: {val_formatted}"))
+
+        children = non_timeline + children
 
         return html.Div(children)
 
